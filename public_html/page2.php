43faebe29e55a6ae327732dfbd618e27687553b4
BananaWizard62 <timmy.dubovik62@gmail.com>
2016-09-22 11:24:31 -0400

    page 2 of index.php

---
+++ public_html/page2.php
@@ -0,0 +1,16 @@
+Hi from page 2
+<?php
+if (isset ($_GET['val']))
+{
+$var = $_GET['val'];
+foreach ($var as $value);
+echo $value."<br />";
+}
+else 
+{
+echo "sorry Dude<br />";
+}
+echo "Hello ";
+$sel = $_GET['orange'];
+echo $sel;
+?>
